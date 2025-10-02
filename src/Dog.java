e3d0f75cfc4540d7dec62a0ba0806b2e0da04809
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2025-10-02 14:49:59 -0700

    Removed flavor text

--- src/Dog.java
+++ src/Dog.java
@@ -1,10 +1,5 @@
 class Dog implements Animal {
 
-    /**
-     * Returns the ASCII art representation of the duck.
-     *
-     * @return A string containing the ASCII art of the duck.
-     */
     @Override
     public String getAnimalArt() {
         return"        \\   .\n" +
@@ -13,12 +8,6 @@ class Dog implements Animal {
 "              ||   ||\n";
     }
   
-    /**
-     * Returns the name of the animal ("duck").
-     * This method overrides the toString method to return the name of the duck.
-     *
-     * @return The string "duck" representing the name of the animal.
-     */
     @Override
     public String toString() {
       return "dog";
